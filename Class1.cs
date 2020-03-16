e72449922abd380d07712835abb4e64ffabb23c3
rajesh prajapati <0216rajesh@gmail.com>
2020-03-16 17:55:20 +0530

    Commit

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static ConsoleApp1.Program;
+
+namespace ConsoleApp1
+{
+    public class Class1
+    {
+        public  void PassingDelegateAsAArgs(displayDelegate del)
+        {
+            del("Hello");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConsoleApp1
+{
+   public class Program
+    {
+        public delegate void displayDelegate(string msg);
+        
+        static void Main(string[] args)
+        {
+            int c = 10;
+
+            c <<= 4;
+
+            Console.WriteLine("Hello World!"+c);
+
+            Dictionary<int,string> obj = new Dictionary<int, string>();
+
+            obj.Add(1,"Rajesh");
+
+            obj.Add(2,"Gopal"); obj.Add(3,"Mahi"); obj.Add(4,"Akash");
+
+            Console.WriteLine("Hello World!" + obj[3]);
+
+            displayDelegate displaydelegate;
+
+            displaydelegate = display;         
+
+            displaydelegate += display2;
+
+            //displaydelegate += display3;
+
+
+            // displaydelegate("Hello Rajesh");
+            Class1 t = new Class1();
+
+            t.PassingDelegateAsAArgs(displaydelegate);
+
+
+
+        }
+
+        public static void display(string msg)
+        {
+            Console.WriteLine(msg+"dfsghfd");
+            Console.ReadLine();
+        }
+
+
+        public static void display2(string msg)
+        {
+            Console.WriteLine(msg+"g");
+            Console.ReadLine();
+        }
+
+        //public static void display3(string msg)
+        //{
+        //    Console.WriteLine(msg);
+        //}
+    }
+}
